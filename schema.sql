ffac5d2f40507cac2e7927b5e66764092ba9611f
TopGek99 <a1767142@student.adelaide.edu.au>
2021-03-02 00:51:45 +1030

    started database

--- schema.sql
+++ schema.sql
@@ -7,5 +7,19 @@ USE employee_trackerDB;
 CREATE TABLE employee (
     id INTEGER PRIMARY KEY AUTOINCREMENT;
     first_name VARCHAR(30);
-    
-)
+    last_name VARCHAR(30);
+    role_id INTEGER FOREIGN KEY (role_);
+    manager_id INTEGER FOREIGN KEY (employee);
+);
+
+CREATE TABLE role_ (
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    title VARCHAR(30);
+    salary DECIMAL;
+    department_id INTEGER FOREIGN KEY (department);
+);
+
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY AUTOINCREMENT;
+    name VARCHAR(30);
+);
---
+++ seed.sql
@@ -0,0 +1,6 @@
+USE employee_trackerDB;
+
+INSERT INTO department ()
+
+INSERT INTO role_ (title,salary,department_id)
+VALUES ('Sales Lead',150000,)
